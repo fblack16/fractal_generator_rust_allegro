049589d89796bd2d88361a323bb0f6be75a96dfe
Felix Black <fblack16@posteo.de>
2022-04-16 21:49:33 +0200

    Added implementation of Letter trait for usize.

--- src/letter.rs
+++ src/letter.rs
@@ -1,3 +1,14 @@
+use crate::word::Word;
+
 pub trait Letter {
-    fn combine(&self, other: &Self);
+    fn combine(&self, other: &Self) -> Word<Self> where Self: Sized;
+}
+
+impl Letter for usize {
+    fn combine(&self, other: &Self) -> Word<Self>
+    {
+        let mut word = Word::new();
+        word.push(self + other);
+        word
+    }
 }
