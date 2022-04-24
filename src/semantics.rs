ee54299a8558bf1e399496176facf69af511262d
Felix Black <fblack@posteo.de>
2022-04-24 16:56:53 +0200

    Changed Semantics to contain one method: execute(&self). Added Koch enum. Implemented Semantics for Koch.

--- src/semantics.rs
+++ src/semantics.rs
@@ -1,1 +1,21 @@
-pub trait Semantics {}
+pub trait Semantics
+{
+    fn execute(&self);
+}
+
+pub enum Koch {
+    Forward,
+    TurnLeft,
+    TurnRight,
+}
+
+impl Semantics for Koch {
+    fn execute(&self) {
+        match self {
+            Self::Forward => { println!("Forward "); },
+            Self::TurnLeft => { println!("TurnLeft "); },
+            Self::TurnRight => { println!("TurnRight "); },
+        }
+    }
+}
+
