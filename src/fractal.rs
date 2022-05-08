c01817b643e63d9a9478c4c385d9bee8ba159db3
Felix Black <fblack@posteo.de>
2022-05-08 21:12:08 +0200

    Comment out large parts of fractal.rs.

--- src/fractal.rs
+++ src/fractal.rs
@@ -7,27 +7,25 @@ use crate::dictionary::Dictionary;
 
 use crate::semantics::Koch;
 
-pub struct Fractal<L, S>
+pub struct Fractal<L>
 where
     L: Letter,
-    S: Semantics,
 {
-        alphabet: Alphabet<L>,
-        dictionary: Dictionary<L, S>,
+        //alphabet: Alphabet<L>,
+        //dictionary: Dictionary<L, S>,
         replacement_rules: ReplacementRules<Word<L>, Word<L>>,
         starting_word: Word<L>,
 }
 
-impl<L, S> Fractal<L, S>
+impl<L> Fractal<L>
 where
     L: Letter,
-    S: Semantics,
 {
     pub fn new() -> Self
     {
         Fractal {
-            alphabet: Alphabet::new(),
-            dictionary: Dictionary::new(),
+            //alphabet: Alphabet::new(),
+            //dictionary: Dictionary::new(),
             replacement_rules: ReplacementRules::new(),
             starting_word: Word::new(),
         }
@@ -38,29 +36,29 @@ where
         self.starting_word.apply_replacement_rules(&self.replacement_rules);
     }
 
-    pub fn apply_semantics(&self)
-    {
-        // Here lies the problem. We need to iterate over all possible subwords that have
-        // semantics (given by the dictionary) that are contained in starting_word and execute
-        // the semantics. This would work fine as long as the subwords are just words that contain 
-        // exactly one letter, but this will not necessarily be the case, especially not for
-        // context dependent Lindenmayer systems. For these, we would need to start searching for
-        // the longest words that have semantics first, working our way down to words with only one 
-        // letter.
-        // -> We need to sort our Dictionary by the length of the words.
-        //
-        // For now, implement it only for words with a single letter (no context dependent
-        // languages)
+    //pub fn apply_semantics(&self)
+    //{
+    //    // Here lies the problem. We need to iterate over all possible subwords that have
+    //    // semantics (given by the dictionary) that are contained in starting_word and execute
+    //    // the semantics. This would work fine as long as the subwords are just words that contain 
+    //    // exactly one letter, but this will not necessarily be the case, especially not for
+    //    // context dependent Lindenmayer systems. For these, we would need to start searching for
+    //    // the longest words that have semantics first, working our way down to words with only one 
+    //    // letter.
+    //    // -> We need to sort our Dictionary by the length of the words.
+    //    //
+    //    // For now, implement it only for words with a single letter (no context dependent
+    //    // languages)
 
-        for &letter in &self.starting_word {
-            let letter_as_word: Word<L> = letter.into();
-            for (word, semantics) in &self.dictionary {
-                if word == &letter_as_word {
-                    semantics.execute();
-                }
-            }
-        }
-    }
+    //    for &letter in &self.starting_word {
+    //        let letter_as_word: Word<L> = letter.into();
+    //        for (word, semantics) in &self.dictionary {
+    //            if word == &letter_as_word {
+    //                semantics.execute();
+    //            }
+    //        }
+    //    }
+    //}
 }
 
 // TESTS
@@ -69,41 +67,41 @@ where
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_apply_replacement_rules() {
-        let mut fractal: Fractal<char, Koch> = Fractal::new();
-        fractal.alphabet.insert('F');
-        fractal.alphabet.insert('+');
-        fractal.alphabet.insert('-');
+    //#[test]
+    //fn test_apply_replacement_rules() {
+    //    let mut fractal: Fractal<char, Koch> = Fractal::new();
+    //    fractal.alphabet.insert('F');
+    //    fractal.alphabet.insert('+');
+    //    fractal.alphabet.insert('-');
 
-        fractal.dictionary.insert("F".into(), Koch::Forward);
-        fractal.dictionary.insert("+".into(), Koch::TurnLeft);
-        fractal.dictionary.insert("-".into(), Koch::TurnRight);
+    //    fractal.dictionary.insert("F".into(), Koch::Forward);
+    //    fractal.dictionary.insert("+".into(), Koch::TurnLeft);
+    //    fractal.dictionary.insert("-".into(), Koch::TurnRight);
 
-        fractal.replacement_rules.insert("F".into(), "F+F--F+F".into());
-        fractal.starting_word = "F".into();
+    //    fractal.replacement_rules.insert("F".into(), "F+F--F+F".into());
+    //    fractal.starting_word = "F".into();
 
-        fractal.apply_replacement_rules();
-        assert_eq!(fractal.starting_word, "F+F--F+F".into());
-    }
+    //    fractal.apply_replacement_rules();
+    //    assert_eq!(fractal.starting_word, "F+F--F+F".into());
+    //}
 
-    #[test]
-    fn test_apply_semantics() {
-        let mut fractal: Fractal<char, Koch> = Fractal::new();
-        fractal.alphabet.insert('F');
-        fractal.alphabet.insert('+');
-        fractal.alphabet.insert('-');
+    //#[test]
+    //fn test_apply_semantics() {
+    //    let mut fractal: Fractal<char, Koch> = Fractal::new();
+    //    fractal.alphabet.insert('F');
+    //    fractal.alphabet.insert('+');
+    //    fractal.alphabet.insert('-');
 
-        fractal.dictionary.insert("F".into(), Koch::Forward);
-        fractal.dictionary.insert("+".into(), Koch::TurnLeft);
-        fractal.dictionary.insert("-".into(), Koch::TurnRight);
+    //    fractal.dictionary.insert("F".into(), Koch::Forward);
+    //    fractal.dictionary.insert("+".into(), Koch::TurnLeft);
+    //    fractal.dictionary.insert("-".into(), Koch::TurnRight);
 
-        fractal.replacement_rules.insert("F".into(), "F+F--F+F".into());
+    //    fractal.replacement_rules.insert("F".into(), "F+F--F+F".into());
 
-        fractal.starting_word = "F".into();
-        fractal.apply_semantics();
+    //    fractal.starting_word = "F".into();
+    //    fractal.apply_semantics();
 
-        fractal.apply_replacement_rules();
-        fractal.apply_semantics();
-    }
+    //    fractal.apply_replacement_rules();
+    //    fractal.apply_semantics();
+    //}
 }
