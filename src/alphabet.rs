9519e737d63eb75ccb9ac62435ef39091ea11514
Felix Black <fblack@posteo.de>
2022-05-08 20:39:49 +0200

    Add From, FromIterator and IntoIterator as well as tests for alphabet.rs.

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 use crate::letter::Letter;
 use std::ops::{Deref, DerefMut};
+use std::convert::From;
 
 pub struct Alphabet<L>
 where
@@ -37,5 +38,108 @@ where
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.container
+    }
+}
+
+// Create an Alphabet from a single letter.
+impl<L> From<L> for Alphabet<L>
+where
+    L: Letter,
+{
+    fn from(letter: L) -> Self {
+        let mut container = HashSet::new();
+        container.insert(letter);
+        Alphabet {
+            container,
+        }
+    }
+}
+
+impl From<&str> for Alphabet<char>
+{
+    fn from(letters: &str) -> Self {
+        Alphabet {
+            container: letters.chars().collect(),
+        }
+    }
+}
+
+// Iterator over owned values
+impl<L> IntoIterator for Alphabet<L>
+where
+    L: Letter,
+{
+    type Item = L;
+    type IntoIter = std::collections::hash_set::IntoIter<L>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.container.into_iter()
+    }
+}
+
+// Iterator over references
+impl<'a, L> IntoIterator for &'a Alphabet<L>
+where
+    L: Letter,
+{
+    type Item = &'a L;
+    type IntoIter = std::collections::hash_set::Iter<'a, L>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.container.iter()
+    }
+}
+
+// Create an Alphabet from an iterator over letters.
+impl<L> FromIterator<L> for Alphabet<L>
+where
+    L: Letter,
+{
+    fn from_iter<I: IntoIterator<Item = L>>(iter: I) -> Self {
+        Alphabet {
+            container: iter.into_iter().collect(),
+        }
+    }
+}
+
+// Create an Alphabet from an itertor over references to letters.
+impl<'a, L> FromIterator<&'a L> for Alphabet<L>
+where
+    L: Letter,
+{
+    fn from_iter<I: IntoIterator<Item = &'a L>>(iter: I) -> Self {
+        Alphabet {
+            container: iter.into_iter().map(|&letter| letter).collect(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_vector() {
+        let chars = vec!['a', 'b', 'c'];
+        let mut alphabet = Alphabet::from_iter(chars);
+        assert!(alphabet.take(&'a').is_some());
+        assert!(alphabet.take(&'b').is_some());
+        assert!(alphabet.take(&'c').is_some());
+    }
+
+    #[test]
+    fn test_from_slice() {
+        let chars: &[char] = &['a', 'b', 'c'];
+        let mut alphabet = Alphabet::from_iter(chars);
+        assert!(alphabet.take(&'a').is_some());
+        assert!(alphabet.take(&'b').is_some());
+        assert!(alphabet.take(&'c').is_some());
+    }
+
+    #[test]
+    fn test_from_string() {
+        let text = "abc";
+        let mut alphabet = Alphabet::from(text);
+        assert!(alphabet.take(&'a').is_some());
+        assert!(alphabet.take(&'b').is_some());
+        assert!(alphabet.take(&'c').is_some());
     }
 }
