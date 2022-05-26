30b0386f4e17771f8b94dfe49bbf1c0ceab70356
Felix Black <fblack@posteo.de>
2022-05-26 15:50:32 +0200

    Rewrite semantics from scratch.

--- src/semantics.rs
+++ src/semantics.rs
@@ -1,21 +1,47 @@
-pub trait Semantics
+use std::ops::{Deref, DerefMut};
+use std::collections::HashMap;
+use crate::word::Word;
+use crate::letter::Letter;
+
+pub trait Payload {}
+
+pub struct Semantics<L, P>
+where
+    L: Letter,
+    P: Payload,
 {
-    fn execute(&self);
+    hash_map: HashMap<Word<L>, fn(&Word<L>, &P)>,
 }
 
-pub enum Koch {
-    Forward,
-    TurnLeft,
-    TurnRight,
-}
-
-impl Semantics for Koch {
-    fn execute(&self) {
-        match self {
-            Self::Forward => { println!("Forward "); },
-            Self::TurnLeft => { println!("TurnLeft "); },
-            Self::TurnRight => { println!("TurnRight "); },
+impl<L, P> Semantics<L, P>
+where
+    L: Letter,
+    P: Payload,
+{
+    pub fn new() -> Self {
+        Semantics {
+            hash_map: HashMap::new(),
         }
     }
 }
 
+impl<L, P> Deref for Semantics<L, P>
+where
+    L: Letter,
+    P: Payload,
+{
+    type Target = HashMap<Word<L>, fn(&Word<L>, &P)>;
+    fn deref(&self) -> &Self::Target {
+        &self.hash_map
+    }
+}
+
+impl<L, P> DerefMut for Semantics<L, P>
+where
+    L: Letter,
+    P: Payload,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.hash_map
+    }
+}
