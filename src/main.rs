7de9e46518991aa2ca3699d6bec74e4e59c3e230
Felix Black <fblack16@posteo.de>
2022-06-03 21:10:34 +0200

    Add template for a simple allegro application in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate allegro;
+
 mod letter;
 mod word;
 mod replacement_rules;
@@ -6,6 +8,35 @@ mod alphabet;
 mod dictionary;
 mod fractal;
 
-fn main() {
-    println!("Hello, world!");
+use allegro::*;
+
+allegro_main!
+{
+    let core = Core::init().unwrap();
+
+    let display = Display::new(&core, 800, 600).unwrap();
+    let timer = Timer::new(&core, 1.0 / 60.0).unwrap();
+    let queue = EventQueue::new(&core).unwrap();
+
+    queue.register_event_source(display.get_event_source());
+    queue.register_event_source(timer.get_event_source());
+
+    let mut redraw = true;
+    timer.start();
+
+    'exit: loop {
+        if redraw && queue.is_empty()
+        {
+            core.clear_to_color(Color::from_rgb_f(0.0, 0.0, 0.0));
+            core.flip_display();
+            redraw = false;
+        }
+
+        match queue.wait_for_event()
+        {
+            DisplayClose{..} => break 'exit,
+            TimerTick{..} => redraw = true,
+            _ => (),
+        }
+    }
 }
